a46660627e41ca1b1af60153f63711621d503efd
marco17r <53026327+marco17r@users.noreply.github.com>
2019-07-17 20:48:25 -0500

    Add files via upload
    initial files from Kullen

--- Controller.java
+++ Controller.java
@@ -1,11 +1,5 @@
 package controller;
-import model.Model;
-import view.View;
 
-public interface Controller 
-{
-	void setOrder(Order order);
-	Order getOrder();
-	View getView();
-	void setView(View view);
+public class Controller {
+
 }
