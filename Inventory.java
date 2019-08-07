f4ad1422a4c44e45a4e6dc7747bac444c2c537a2
marco17r <53026327+marco17r@users.noreply.github.com>
2019-08-06 21:56:28 -0500

    more changes before file due date

--- Inventory.java
+++ Inventory.java
@@ -4,31 +4,46 @@ import java.math.BigDecimal;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Inventory extends Drink {
-
-    //This class defines the inventory of drinks available to users of the app for
-    //purchase and use of said drinks
-    public enum DRINKS {
-        WATER, LEMONADE, KOMBUCHA, LIMEADE, KOOLADE
-    }
-
-    public Map<String, Drink> mapOfDrinks = new TreeMap<>();
-
-    public Map<String, Drink> storeDefaultDrinkMenu() 
-    {
-        mapOfDrinks.put("Water", new Drink("Water", "Drinking Water", new BigDecimal("0.0")));
-        mapOfDrinks.put("Lemonade", new Drink("Lemonade", "Water with lemon juice and brown sugar", new BigDecimal("1.0")));
+public class Inventory extends Drink
+{
+	//This class defines the inventory of drinks available to users of the app for
+	//purchase and use of said drinks
+	public enum drinks 
+	{
+		WATER, LEMONADE, KOMBUCHA, LIMEADE, KOOLADE
+	}
+	
+	//Standard drink inventory Array
+	Drink mapOfdrinkss[] = 
+	{
+			new Drink("Water", "Drinking Water", new BigDecimal("0.0")),
+			new Drink("Lemonade", "Water with lemon juice and brown sugar", new BigDecimal("1.0")),
+			new Drink("Kombucha", "Probiotic drink from India, great for gut flora", new BigDecimal("4.0")),
+			new Drink("Limeade", "Water with lime juice and brown sugar", new BigDecimal("1.0")),
+			new Drink("Koolade", "Water stirred with Koolade drink mix", new BigDecimal("2.0"))
+	};
+	
+	public Map<String,Drink> mapOfDrinks = new TreeMap<>();
+	
+	public Map<String,Drink> defaultDrinkMenu()
+	{
+		mapOfDrinks.put("Water", new Drink("Water", "Drinking Water", new BigDecimal("0.0")));
+		mapOfDrinks.put("Lemonade", new Drink("Lemonade", "Water with lemon juice and brown sugar", new BigDecimal("1.0")));
         mapOfDrinks.put("Kombucha", new Drink("Kombucha", "Probiotic drink from India, great for gut flora", new BigDecimal("4.0")));
         mapOfDrinks.put("Limeade", new Drink("Limeade", "Water with lime juice and brown sugar", new BigDecimal("1.0")));
         mapOfDrinks.put("Koolade", new Drink("Koolade", "Water stirred with Koolade drink mix", new BigDecimal("1.0")));
         return mapOfDrinks;
+	}
+	
+	//Mutator to add new drinks to the default drink menu.
+	public void addNewDrink(Drink drinkObject)
+	{
+        mapOfDrinks.put(drinkObject.getName(), drinkObject);
     }
-
-        public void addNewDrink(Drink drink){
-            mapOfDrinks.put(drink.getName(), drink);
-        }
-
-        public Drink getDrinkFromName(String drinkName){
-            return mapOfDrinks.get(drinkName);
-        }
+	
+	//Accessor Method to get drink info from the default drink menu.
+    public Drink getDrinkFromName(String drinkObjectName)
+    {
+        return mapOfDrinks.get(drinkObjectName);
+    }
 }
--- StartUpWindow.java
+++ StartUpWindow.java
@@ -1,12 +1,17 @@
 package view;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.awt.Color;
+import java.awt.Paint;
+import java.awt.color.*;
 
 //Class displays the main window
 public class StartUpWindow extends Application
@@ -17,83 +22,108 @@ public class StartUpWindow extends Application
 	//@param stage
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception 
+	public void start(Stage initStage) throws Exception 
 	{
-		startUpWindow = primaryStage;
-		startUpWindow.setTitle("Login Page");
+		startUpWindow = initStage;
+		//Sets the title of the start up window
+		
 		startUpWindow.setOnCloseRequest(event ->
 		{
 			event.consume();
 		});
-
-		Button memberSignInButton = new Button("Membership Sign-In");
+		
+		//Button declarations for the start up page
 		Button memberLoginButton = new Button("Membership Login");
+		Button memberSignInButton = new Button("Membership Sign-Up");
 		Button guestLoginButton = new Button("Guest Order");
 		Button drinkMenuButton = new Button("Drink Menu");
 		Button exitButton = new Button("Exit");
 		
+		//Declare an horizontal box to put buttons in
+		HBox horizontalButtonBox = new HBox();
+		//Retrieving the observable list of buttons getting added to the HBox
+		ObservableList list = horizontalButtonBox.getChildren();
+		//Adding all the nodes to the observable list
+		list.addAll(memberLoginButton, memberSignInButton, guestLoginButton, drinkMenuButton, exitButton);
+		//Aligns the button box to the bottom center of the screen
+		horizontalButtonBox.setAlignment(Pos.BOTTOM_CENTER);
 		
-		HBox horizontalBox = new HBox();
-		horizontalBox.getChildren().addAll(memberSignInButton, memberLoginButton, guestLoginButton, drinkMenuButton, exitButton);
-		horizontalBox.setAlignment(Pos.BOTTOM_CENTER);
-
 		BorderPane bp = new BorderPane();
 		bp.setId("StartUpWindow");
-		bp.setCenter(horizontalBox);
+		bp.setCenter(horizontalButtonBox);
 		
-		Scene startUpScene =  new Scene(bp,700,373);
-		startUpScene.getStylesheets().add("displayStyle.css");
+		//Styling Button Objects
+		memberSignInButton.setStyle("-fx-background-color: orangered; -fx-text-fill: white;"); 
+		memberLoginButton.setStyle("-fx-background-color: navy; -fx-text-fill: white;"); 
+		guestLoginButton.setStyle("-fx-background-color: magenta; -fx-text-fill: white;"); 
+		drinkMenuButton.setStyle("-fx-background-color: red; -fx-text-fill: white;"); 
+		exitButton.setStyle("-fx-background-color: indigo; -fx-text-fill: white;"); 
+		  
+	    horizontalButtonBox.setStyle("-fx-background-color: aqua;"); 
 		
+	    //Creating a scene object
+		Scene startUpScene =  new Scene(bp, 700, 353);
+		//startUpScene.getStylesheets().add("displayStyle.css");
+		//Sets the title of the start up window
+		startUpWindow.setTitle("Amazing Barista!");
+		//Adding the scene to the stage
 		startUpWindow.setScene(startUpScene);
+		//Displays the content of the window stage
 		startUpWindow.show();
 		
-		
-		memberLoginButton.setOnAction(event->
+		//Member log-in action event to sign in to the member account
+		memberLoginButton.setOnAction(event-> 
 		{
 			if (event.getSource() == memberLoginButton)
 			{
-				MemberLogin.displayLogIn();
-				startUpWindow.close();
+				ViewHandler.displayLogIn();
 			}
 		});
 		
-		memberSignInButton.setOnAction(event->
+		//Member Sign-in action event to sign in new members
+		memberSignInButton.setOnAction(event-> 
 		{
 			if(event.getSource() == memberSignInButton)
 			{
 				ViewHandler.displaySignUp();
 			}
+		
 		});
 		
-		guestLoginButton.setOnAction(event->
+		//Guest Order action event to order with member discount
+		guestLoginButton.setOnAction(event-> 
 		{
-			if(event.getSource() == guestLoginButton)
+			if (event.getSource() == guestLoginButton)
 			{
+				//ViewHandler.startOrder("Enter Name");
 				GuestLogin guestLogin = new GuestLogin();
 				guestLogin.displayMenu();
 			}
+			
 		});
-
-		drinkMenuButton.setOnAction(event->
+		
+		//Drink Menu action event to display the drink menu
+		drinkMenuButton.setOnAction(event-> 
 		{
 			if (event.getSource() == drinkMenuButton)
 			{
 				ViewHandler.displayDrinkMenu();
 				MenuWindow displayMenu = new MenuWindow();
-				displayMenu.menuDisplayWindow("General ");
+				displayMenu.menuDisplayPage("General ");
 			}
 		});
-		
+		//Exit action event that closes the application window
 		exitButton.setOnAction(event->
 		{
-			if (event.getSource() == exitButton)
+			if(event.getSource() == exitButton)
 			{
 				startUpWindow.close();
 				event.consume();
 			}
-		});		
+		});
 	}
 
+	
 	public static void main(String[] args)
 	{
 		launch(args);
