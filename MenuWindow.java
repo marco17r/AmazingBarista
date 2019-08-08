cc311989be0c215b247c727b7f6247b9fa396378
marco17r <53026327+marco17r@users.noreply.github.com>
2019-08-07 21:58:38 -0500

    final

--- MenuWindow.java
+++ MenuWindow.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -28,112 +30,130 @@ public class MenuWindow
     private Map<String, Button> mapOfAddButtons = new HashMap<>();
     private Map<String, Button> mapOfClearButtons = new HashMap<>();
 
-    public void menuDisplayWindow(String userType) 
+    public void menuDisplayPage(String userType, boolean trueMember) 
     {
         Stage displayMenuWindow = new Stage();
-        //Block events to other windows
-        displayMenuWindow.initModality(Modality.APPLICATION_MODAL);
-        displayMenuWindow.setTitle(userType + " Menu Page");
-        displayMenuWindow.setMinWidth(1000);
-        displayMenuWindow.setMaxWidth(1200);
-
-        BorderPane menuDisplayBPane = new BorderPane();
-        menuDisplayBPane.setPadding(new Insets(20, 20, 20, 20));
-        menuDisplayBPane.getChildren().add(createDrinkTitle("Drink", "Description", "Price", "Quantity"));
-        //layout.setAlignment(Pos.TOP_CENTER);
+        //Instantiating the VBox class
+        VBox verticleBoxLayout = new VBox(10);
         Inventory inv = new Inventory();
-
-        Map<String, Drink> mapOfDrinks = inv.storeDefaultDrinkMenu();
         Order customerOrder = new Order();
         Text totalPriceText = new Text();
+        //Declaration of the submit button
+        Button submitButton = new Button("Submit");
+        
+        //Block events to other windows
+        displayMenuWindow.initModality(Modality.APPLICATION_MODAL);
+        
+        //Setting the margins to the vertical box layout
+        verticleBoxLayout.setPadding(new Insets(20, 20, 20, 20));
+        verticleBoxLayout.getChildren().add(createDrinkTitle("Drink", "Description", "Price", "Quantity"));
+        //Retrieving the observable list of the VBox
+        ObservableList list = verticleBoxLayout.getChildren();
+        //Adding the Title HBox to the observable list
+        //list.add(createDrinkTitle("Drink", "Description", "Price", "Quantity"));
+        
+        Map<String, Drink> mapOfDrinks = inv.defaultDrinkMenu();
 
         for (String itemName : mapOfDrinks.keySet()) 
         {
-            Drink drink = mapOfDrinks.get(itemName);
-            menuDisplayBPane.getChildren().addAll(createDrinkMenuItems(drink.getName(), drink.getDescription(), String.valueOf(drink.getPrice())));
-            //verticleBoxLayOut.setAlignment(Pos.CENTER_LEFT);
+            Drink drinkInfo = mapOfDrinks.get(itemName);
+            verticleBoxLayout.getChildren().addAll(createDrinkMenuItems(drinkInfo.getName(), drinkInfo.getDescription(), String.valueOf(drinkInfo.getPrice())));
         }
 
         for (String drinkName : mapOfAddButtons.keySet()) 
         {
-            mapOfAddButtons.get(drinkName).setOnAction(e -> 
+            mapOfAddButtons.get(drinkName).setOnAction(event -> 
             {
                     Drink drink = mapOfDrinks.get(drinkName);
                     BigDecimal itemQuantity = new BigDecimal(mapOfTextInputs.get(drink.getName()).getText());
                     customerOrder.addToOrder(drink, itemQuantity);
-                    totalPriceText.setText("$" + String.valueOf(customerOrder.getOrderTotal()));
+                    
+                    if(trueMember == true)
+                    {
+                    	BigDecimal newMemberDivisor = new BigDecimal("2");
+                    	BigDecimal newMemberPrice = new BigDecimal(String.valueOf(customerOrder.getOrderTotal().divide(newMemberDivisor)));
+                    	totalPriceText.setText(("$" + newMemberPrice));
+                    }
+                    else
+                    {
+                    	totalPriceText.setText("$" + String.valueOf(customerOrder.getOrderTotal()));
+                    }
             });
         }
 
         for (String drinkName : mapOfClearButtons.keySet()) 
         {
-            mapOfClearButtons.get(drinkName).setOnAction(e -> 
+            mapOfClearButtons.get(drinkName).setOnAction(event -> 
             {
                     customerOrder.clearOrder();
                     totalPriceText.setText(String.valueOf(customerOrder.getOrderTotal()));
             });
         }
         
-
-        Button submitButton = new Button("Submit");
-        submitButton.setOnAction(e -> 
+        //Submit button action event
+        submitButton.setOnAction(event -> 
         {
             displayMenuWindow.close();
         });
 
-        menuDisplayBPane.getChildren().addAll(submitButton, totalPriceText);
-        //layout.setAlignment(Pos.TOP_LEFT);
-        //Display login window and wait for it to be cancelled before returning.
-        Scene menuDisplayScene = new Scene(menuDisplayBPane);
-        displayMenuWindow.setScene(menuDisplayScene);
+        verticleBoxLayout.getChildren().addAll();
+        //Adding the node to the observable list
+        list.addAll(submitButton, totalPriceText);
+        
+        verticleBoxLayout.setStyle("-fx-background-color: AQUA;");
+        //Setting title to the stage.
+        displayMenuWindow.setTitle(userType + " Menu Page");
+        displayMenuWindow.setMinWidth(1200);
+        //displayMenuWindow.setMaxWidth(1000);
+        //Creating a scene object
+        Scene displayMenuScene = new Scene(verticleBoxLayout);
+        //Adding scene to the stage
+        displayMenuWindow.setScene(displayMenuScene);
+        //Display Menu window and wait for it to be cancelled before returning.
         displayMenuWindow.showAndWait();
     }
 
     private static HBox createDrinkTitle(String drinkTitle, String descriptionTitle, String priceTitle, String quantityTitle) 
     {
-        HBox horizontalBox = new HBox();
-        horizontalBox.getChildren().add(new Label(drinkTitle));
-        horizontalBox.getChildren().add(new Label(descriptionTitle));
-        horizontalBox.getChildren().add(new Label(priceTitle));
-        horizontalBox.getChildren().add(new Label(quantityTitle));
-        horizontalBox.setSpacing(111);
-        horizontalBox.setAlignment(Pos.TOP_LEFT);
-        return horizontalBox;
+        HBox topHorizontalBox = new HBox();
+        topHorizontalBox.getChildren().addAll(new Label(drinkTitle), new Label(descriptionTitle), new Label(priceTitle), new Label(quantityTitle));
+        //ObservableList<Node> list = topHorizontalBox.getChildren();
+        //Adding the node to the observable list
+        //list.addAll(new Label(drinkTitle), new Label(descriptionTitle), new Label(priceTitle), new Label(quantityTitle));
+        //Styling Horizontal Box Layout
+        topHorizontalBox.setStyle("-fx-background-color:orangered;");
+        topHorizontalBox.setSpacing(111);
+        topHorizontalBox.setAlignment(Pos.TOP_LEFT);
+        
+        return topHorizontalBox;
     }
 
-    @SuppressWarnings("restriction")
-	public BorderPane createDrinkMenuItems(String drinkTitleItem, String drinkDescriptionItem, String drinkPriceItem) 
+    public HBox createDrinkMenuItems(String drinkTitleItem, String drinkDescriptionItem, String drinkPriceItem) 
     {
-    	BorderPane bPane = new BorderPane();
-    	HBox horizontalLabelBox = new HBox();
-        horizontalLabelBox.getChildren().add(new Label(drinkTitleItem));
-        horizontalLabelBox.getChildren().add(new Label(drinkDescriptionItem));
-        horizontalLabelBox.getChildren().add(new Label(drinkPriceItem));
-        bPane.getChildren().add(horizontalLabelBox);
-        
-        
-        TextField quantity = new TextField();
-        bPane.getChildren().add(quantity);
-        mapOfTextInputs.put(drinkTitleItem, quantity);
-        
-        HBox horizontalButtonBox = new HBox();
+        HBox bottomHorizontalBox = new HBox();
+        TextField drinkQuality = new TextField();
         Button addToCartButton = new Button("Add To Cart");
-        horizontalButtonBox.getChildren().add(addToCartButton);
-        mapOfAddButtons.put(drinkTitleItem, addToCartButton);
-
         Button clearCartButton = new Button("Clear Cart");
-        horizontalButtonBox.getChildren().add(clearCartButton);
-        bPane.getChildren().add(horizontalButtonBox);
+        
+        bottomHorizontalBox.getChildren().addAll(new Label(drinkTitleItem), new Label(drinkDescriptionItem), new Label(drinkPriceItem), drinkQuality, addToCartButton, clearCartButton);
+        //Retrieving the observable list of the VBox
+        //ObservableList list = bottomHorizontalBox.getChildren();
+        //Adding the node to the observable list
+        //list.addAll(drinkQuality, addToCartButton, clearCartButton);
+        
+        mapOfTextInputs.put(drinkTitleItem, drinkQuality);
+        mapOfAddButtons.put(drinkTitleItem, addToCartButton);
         mapOfClearButtons.put(drinkTitleItem, clearCartButton);
         
-        //Styling nodes  
-        bPane.setLeft(horizontalLabelBox);
-        bPane.setCenter(quantity);
-        bPane.setRight(horizontalButtonBox);
-        addToCartButton.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
-        clearCartButton.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
-        quantity.setStyle("-fx-font: semi bold 11px 'times new roman' "); 
-        horizontalLabelBox.setStyle("-fx-background-color:BEIGE;");
-        return bPane;
+        //Styling Horizontal Box Layout  
+        addToCartButton.setStyle("-fx-background-color: green; -fx-text-fill: white;"); 
+        clearCartButton.setStyle("-fx-background-color: TOMATO; -fx-text-fill: white;"); 
+        drinkQuality.setStyle("-fx-font: semi bold 11px 'times new roman' "); 
+        
+        bottomHorizontalBox.setStyle("-fx-background-color: cyan;");
+        bottomHorizontalBox.setSpacing(70);
+        bottomHorizontalBox.setAlignment(Pos.CENTER);
+        
+        return bottomHorizontalBox;
     }
 }
